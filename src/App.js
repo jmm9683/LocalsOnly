535554a58bf2ac4bf987ea78810689da52e51da2
Jake Morrissey <jakemorrissey@utexas.edu>
2022-02-16 20:21:38 -0600

    title, description, geohash, category

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
@@ -9,6 +9,8 @@ import 'firebase/compat/analytics';
 
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
+ 
+const geofire = require('geofire-common');
 
 firebase.initializeApp({
   apiKey: "AIzaSyDjt6b4uyV7S1Fgwz_au0Q6IUu1lUEwEhw",
@@ -68,43 +70,93 @@ function SignOut() {
 
 function HomePage() {
 
+  const [showStashForm, setShowStashForm] = useState(false)
+  const toggleStashForm = () => setShowStashForm(!showStashForm)
+
   const messagesRef = firestore.collection('userStashes');
 
   const [position, setPosition] = useState('');
+  const [positionHash, setPositionHash] = useState('');
+
+  const [category, setCategory] = useState('any');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
    
+  const [posting, setPosting] = useState(false);
+  useEffect(() => {
+    if (posting) {
+      savePosition();
+    }
+  }, [posting]);
+
   function getCurrentPosition() {
     navigator.geolocation.getCurrentPosition(
       function(position) {
-        console.log(position);
         setPosition(position);
+        setPositionHash(geofire.geohashForLocation([position.coords.latitude,position.coords.longitude]));
+        setPosting(true);
       },
-  
       function(error) {
         console.error("Error Code = " + error.code + " - " + error.message);
         return(error);
       }
     );
+    
   }
 
   const savePosition = async (e) => {
-    getCurrentPosition() 
 
-    const { uid, photoURL } = auth.currentUser;
+    const { uid } = auth.currentUser;
 
     await messagesRef.add({
       latitude: position.coords.latitude,
       longitude: position.coords.longitude,
-      timestamp: position.timestamp,
+      positionHash: positionHash,
+      title: title,
+      category: category,
+      description: description,
+      category_positionHash: category + "_" + positionHash,
       createdAt: firebase.firestore.FieldValue.serverTimestamp(),
       uid
     })
-
+    setPosting(false);
   }
- 
+
+  function postStash(e) {
+    e.preventDefault();
+    getCurrentPosition()
+    toggleStashForm();
+  }
 
   return (<>
     <div>
-      <button className="location" onClick={() => savePosition()}>Get Location</button>
+      { !showStashForm ? <button className="location" onClick={toggleStashForm}>Get Location</button> : null }
+      { showStashForm ? 
+        <div>
+            <div id="stashForm" className="stashForm-container">
+              <form onSubmit={postStash}  onChange={() => {}}>
+                  <label>
+                    Describe Your Find:
+                  </label>
+                  <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+                  <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                      <option value="any">Anything</option>
+                      <option value="drink">Drink</option>
+                      <option value="food">Food</option>
+                      <option value="outdoors">Outdoors</option>
+                    </select>
+                  <textarea
+                    rows="2.5"
+                    cols="15"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                  <input type="submit" value="Submit"/>
+                </form>
+            </div>
+            <button className="location" onClick={toggleStashForm}>Cancel</button> 
+        </div> 
+      : null }
     </div>
   </>)
 }
