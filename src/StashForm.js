1f6aa1cd90cea70c24ef2a097f5faa18f43f9532
jmm9683 <jakemorrissey@utexas.edu>
2022-03-02 21:31:25 -0800

    fix

--- src/StashForm.js
+++ src/StashForm.js
@@ -68,7 +68,7 @@ function StashForm() {
   }
 
   return (
-    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+    <div className="sm:mx-auto sm:w-full sm:max-w-md">
       <div
         id="stashForm"
         className="stashForm-container bg-slate-800 bg-opacity-90 py-8 px-6 shadow rounded-lg sm:px-10"
